ab154ccbef1273e9b4c19985f5d88692c977a9d6
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-06-02 16:40:09 +0700

    menambahkan metode input dan membuat file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main(){
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
-    
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
 }
