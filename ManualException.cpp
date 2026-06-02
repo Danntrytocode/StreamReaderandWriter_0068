08c63d4a3d8df9038cf4edaff82f87df892d91ed
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-06-02 17:01:53 +0700

    add algorithm catch jika throw bertipe int dan float

--- ManualException.cpp
+++ ManualException.cpp
@@ -6,7 +6,17 @@ int main(){
     //membuar isi dari try untuk pengujian error
     try{
         cout << "Selamat belajar di prodi TI UMY" << endl;
-        throw 0.5; //melemparkan sebuah integer maka
+        throw 0.5; //jika melemparkan sebuah integer maka output :selamat,pengecualian dan pernyatan.
+                    //jika melemparkan sebuah float maka output :selamat,default dan pernyataan.
         cout << "Pernyataan tidak akan di tampilkan" << endl;
     }
+    //kondisi jika throw yang bertipe data integer
+    catch (int a){
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    //kondisi jika throw bertipe data selain integer
+    catch (...){
+        cout << "default pengecualian di eksekusi" << endl;
+    }
+    return 0;
 }
